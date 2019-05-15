ee987083c3217b67017d08b78316399f8b586794
Di Diego Alessandro (IA462932TS) <alessandro.didiego@enel.com>
2019-05-15 16:39:08 +0200

    Add better logs

--- sync_gits.ps1
+++ sync_gits.ps1
@@ -14,20 +14,26 @@ foreach ($repo_line in $repo_lines) {
     $repo_list = $repo_line.Split("{,}")
     for ($i=0; $i -lt $repo_list.length; $i++) {
         if ($i -eq 0){
+            Write-Host "Cloning " $repo_list[$i] -ForegroundColor Green
             git clone --bare $repo_list[$i] $temp_path
             cd $temp_path
         }
         else {
+            Write-Host "Adding remote " $repo_list[$i] -ForegroundColor Green
             git remote add --mirror=fetch "repo_$i" $repo_list[$i]
         }
     }
+    Write-Host "Fetching " $repo_list[0] -ForegroundColor Green
     git fetch origin --tags
     for ($i=1; $i -lt $repo_list.length; $i++) {
+        Write-Host "Fetching " $repo_list[$i] -ForegroundColor Green
         git fetch "repo_$i" --tags
     }
+    Write-Host "Pushing " $repo_list[0] -ForegroundColor Green
     git push origin --all
     git push origin --tags
     for ($i=1; $i -lt $repo_list.length; $i++) {
+        Write-Host "Pushing " $repo_list[$i] -ForegroundColor Green
         git push  "repo_$i" --all
         git push  "repo_$i" --tags
     }
